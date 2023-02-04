4444c84d209c65afffc11d7de03d29b1c8ed547e
khushimi123 <123157982+khushimi123@users.noreply.github.com>
2023-02-04 02:33:18 -0500

    INITIAL COMMIT

--- server.js
+++ server.js
@@ -1,11 +1,75 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: khushi ID: 166961219 Date: 01 February 2023
 
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+*
+********************************************************************************/
+const express = require("express");
+const path = require("path");
+const { initialize, getAllPosts, getPublishedPosts, getCategories } = require("./blog-service.js");
+
+const app = express();
+
+app.use(express.static('public')); 
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.get("/", (req,res) => {
+  res.redirect("/about");
 });
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+})
+
+
+app.get("/blog", (req, res) => {
+  getPublishedPosts()
+  .then((data) => {
+    res.send(data)
+  })
+
+  .catch((err) => {
+    res.send(err);
+  })
+});
+
+
+app.get("/posts", (req, res) => {
+  getAllPosts()
+    .then((data) => {
+      res.send(data)
+    })
+   
+    .catch((err) => {
+      res.send(err);
+    })
+});
+
+app.get("/categories", (req, res) => {
+  getCategories()
+  .then((data) => {
+    res.send(data)
+  })
+
+  .catch((err) => {
+    res.send(err);
+  })
+})
+
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "notFoundPage.html"));
+})
+
+
+initialize().then(() => {
+
+  app.listen(HTTP_PORT, () => {
+    console.log("Express http server listening on: " + HTTP_PORT);
+  });
+})
