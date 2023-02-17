3abcd054875a744915d3744cba4b1300c73fa7c3
khushimidha <khushi5@myseneca.ca>
2023-02-17 12:00:43 -0500

    assignment 3

--- server.js
+++ server.js
@@ -1,64 +1,159 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: khushi ID: 166961219 Date: 01 February 2023
+*  Name: khushi ID: 166961219 Date: 17 February 2023
 
-*
+github link : https://github.com/khushimidha/Web322-App.git
+
+
+* cyclic link - https://app.cyclic.sh/#/app/khushimidha-web322-app/overview
+website : https://dark-pear-grasshopper-wear.cyclic.app/about
 ********************************************************************************/
 const express = require("express");
 const path = require("path");
-const { initialize, getAllPosts, getPublishedPosts, getCategories } = require("./blog-service.js");
+const blogService = require("./blog-service.js");
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
 
+cloudinary.config({
+  cloud_name: 'dwp22m3wk',
+  api_key: '278342816363226',
+  api_secret: 'AVa7ypERW_hZvctAR7',
+  secure: true
+});
+
+const upload = multer();
 const app = express();
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   res.redirect("/about");
 });
 
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.sendFile(path.join(__dirname, "views", "about.html"));
 })
 
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addPost.html"));
+})
 
-app.get("/blog", (req, res) => {
-  getPublishedPosts()
-  .then((data) => {
-    res.send(data)
-  })
+app.post('/posts/add', upload.single('featureImage'), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream(
+          (error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          }
+        );
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
 
-  .catch((err) => {
-    res.send(err);
-  })
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req).then((uploaded) => {
+      processPost(uploaded.url);
+    });
+  } else {
+    processPost('');
+  }
+
+  function processPost(imageUrl) {
+    req.body.featureImage = imageUrl;
+    const postData = req.body;
+    blogService.addPost(postData)
+      .then(() => {
+        res.redirect('/posts');
+      })
+      .catch((err) => {
+        console.error(err);
+        res.redirect('/posts');
+      });
+  }
+
 });
 
 
-app.get("/posts", (req, res) => {
-  getAllPosts()
+app.get("/blog", (req, res) => {
+  blogService.getPublishedPosts()
     .then((data) => {
       res.send(data)
     })
-   
+
     .catch((err) => {
       res.send(err);
     })
 });
 
-app.get("/categories", (req, res) => {
-  getCategories()
-  .then((data) => {
-    res.send(data)
-  })
 
-  .catch((err) => {
-    res.send(err);
+app.get("/posts", (req, res) => {
+  const category = req.query.category;
+  const minDateStr = req.query.minDate;
+  let posts;
+
+  if (category) {
+    blogService.getPostsByCategory(category).then((data) => {
+      res.json(data);
+    }).catch(error => {
+    res.json(error);
+  });
+  } else if (minDateStr) {
+   blogService.getPostsByMinDate(minDateStr).then((data) => {
+      res.json(data);
+    }).catch(error => {
+    res.json(error);
+  });
+  } else {
+    posts = blogService.getAllPosts().then((data) => {
+      res.json(data);
+    }).catch(error => {
+    res.json(error);
+  });
+  }
+
+});
+
+app.get("/post/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  blogService
+  .getPostById(id)
+  .then((posts) => {
+    res.json(posts);
   })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).json("Post not found");
+  });
+});
+
+
+app.get("/categories", (req, res) => {
+  blogService.getCategories()
+    .then((data) => {
+      res.send(data)
+    })
+
+    .catch((err) => {
+      res.send(err);
+    })
 })
 
 
@@ -67,9 +162,12 @@ app.use((req, res) => {
 })
 
 
-initialize().then(() => {
+blogService.initialize()
+  .then(() => {
 
   app.listen(HTTP_PORT, () => {
     console.log("Express http server listening on: " + HTTP_PORT);
   });
-})
+}) .catch((error) => {
+  console.error(`Failed to initialize blog service: ${error}`);
+});
